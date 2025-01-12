f1540f6b6d99bd997dc37dfcfe65e4fa495c516d
Amit Maharjan <maharjanamit8@gmail.com>
2025-01-12 14:34:13 -0500

    Remove Duplicates From Sorted Array II

---
+++ src/com/company/TopInterview150/RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,16 @@
+package com.company.TopInterview150;
+
+public class RemoveDuplicatesFromSortedArrayII {
+    public int removeDuplicates(int[] nums) {
+        int size = nums.length;
+        if (size <= 2) return size;
+
+        int pos = 2;
+        for (int i=2; i<nums.length; i++) {
+            if (nums[i] != nums[pos-2]) {
+                nums[pos++] = nums[i];
+            }
+        }
+        return pos;
+    }
+}

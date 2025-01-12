a53aff1279d2a3b5013fa54459466b2818301802
Amit Maharjan <maharjanamit8@gmail.com>
2025-01-11 19:31:41 -0500

    Remove Duplicates From Sorted Array

---
+++ src/com/company/TopInterview150/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,52 @@
+package com.company.TopInterview150;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int pos = nums.length - 1;
+        if (pos < 0) return 0;
+        int element = nums[pos];
+        int uniqueValue = 1;
+
+        for (int i=pos-1; i>=0; i--) {
+            if (nums[i] == element) {
+                continue;
+            } else {
+                if (uniqueValue != 1) {
+                    for (int j=i+1,k=pos; j<=i+uniqueValue; j++,k++) {
+                        nums[j] = nums[k];
+                    }
+                }
+
+                pos = i;
+                element = nums[pos];
+                uniqueValue++;
+            }
+        }
+
+        for (int j=1,k=pos+1; j<uniqueValue; j++,k++) {
+            nums[j] = nums[k];
+        }
+
+        return uniqueValue;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicatesFromSortedArray obj = new RemoveDuplicatesFromSortedArray();
+
+        int[] nums = {1,1,2};
+        int k = obj.removeDuplicates(nums);
+        System.out.println("k = " + k);
+        for (int i = 0; i < k; i++) {
+            System.out.println(nums[i]);
+        }
+
+        System.out.println("--------");
+
+        nums = new int[]{0,0,1,1,1,2,2,3,3,4};
+        k = obj.removeDuplicates(nums);
+        System.out.println("k = " + k);
+        for (int i = 0; i < k; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}

66724ec07d8a3fdd83748478cd1f3f92d96a4337
Amit Maharjan <maharjanamit8@gmail.com>
2025-01-11 19:43:55 -0500

    Faster Remove Duplicates From Sorted Array

--- src/com/company/TopInterview150/RemoveDuplicatesFromSortedArray.java
+++ src/com/company/TopInterview150/RemoveDuplicatesFromSortedArray.java
@@ -30,11 +30,23 @@ public class RemoveDuplicatesFromSortedArray {
         return uniqueValue;
     }
 
+    public int removeDuplicates2(int[] nums) {
+        int pos = 0;
+        for (int i=1; i<nums.length; i++) {
+            if (nums[pos] != nums[i]) {
+                nums[pos+1] = nums[i];
+                pos++;
+            }
+        }
+
+        return ++pos;
+    }
+
     public static void main(String[] args) {
         RemoveDuplicatesFromSortedArray obj = new RemoveDuplicatesFromSortedArray();
 
         int[] nums = {1,1,2};
-        int k = obj.removeDuplicates(nums);
+        int k = obj.removeDuplicates2(nums);
         System.out.println("k = " + k);
         for (int i = 0; i < k; i++) {
             System.out.println(nums[i]);
@@ -43,7 +55,7 @@ public class RemoveDuplicatesFromSortedArray {
         System.out.println("--------");
 
         nums = new int[]{0,0,1,1,1,2,2,3,3,4};
-        k = obj.removeDuplicates(nums);
+        k = obj.removeDuplicates2(nums);
         System.out.println("k = " + k);
         for (int i = 0; i < k; i++) {
             System.out.println(nums[i]);

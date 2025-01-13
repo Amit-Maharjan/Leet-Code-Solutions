da9ea4e6114bbe70dadaa3edce01f343c6afe382
Amit Maharjan <maharjanamit8@gmail.com>
2025-01-13 15:33:42 -0500

    Best Time to Buy and Sell Stock

---
+++ src/com/company/TopInterview150/ArrayString/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+package com.company.TopInterview150.ArrayString;
+
+public class BestTimeToBuyAndSellStock {
+    public int maxProfit(int[] prices) {
+        int buyPrice = prices[0];
+        int profit = 0;
+        for(int i=1; i<prices.length; i++) {
+            if (prices[i] < buyPrice) {
+                buyPrice = prices[i];
+            }
+
+            profit = Math.max(profit, prices[i] - buyPrice);
+        }
+
+        return profit;
+    }
+}
